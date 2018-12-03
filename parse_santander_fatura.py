f87359c798bb744893eb9788c1ec701412a81c66
Diego Nogueira <diego.nogueira@loggi.com>
2018-12-03 14:11:17 -0200

    field breakdown

--- parse_santander_fatura.py
+++ parse_santander_fatura.py
@@ -2,14 +2,31 @@
 
 import re
 
-pattern = re.compile("(\s*(\d{2}\/\d{2})\s+(([\w\*-]+\s?)+)\b\s+(PARC\s+(\d{2}\/\d{2}))?\s+([\d,\-]+))")
-#((\d{2}\/\d{2})\s+([\w\*]+)\s+(PARC\s+(\d{2}\/\d{2}))?\s+([\d,\-]+))+
+transaction_pattern = re.compile("(\s*(\d{2}\/\d{2})\s+([\w\*-]+\s?)+\s+(PARC\s+(\d{2}\/\d{2}))?\s+([\d,\-\.]+))")
+installment_pattern = re.compile("(\d{2})\/\d{2}")
 
 fatura_txt = open("fatura.txt", "r")
 
+#TODO: match multiple occurrences in the same line
+
 line = fatura_txt.readline()
 while line:
-    print line
-    print pattern.match(line)
+    match = transaction_pattern.search(line)
+    if (bool(match)):
+        print "---"
+        print line
+        print "============"
+        print match.group(0)
+        transaction_date = match.group(2)
+        transaction_payee = match.group(3)
+        transaction_value = match.group(6)
+        print "Date: " + transaction_date
+        print "Payee: " + transaction_payee
+        print "Value: " + transaction_value
+        installment = match.group(5)
+	if (bool(installment)):
+            transaction_installment = installment_pattern.match(installment).group(1)
+            print "Installment: " + transaction_installment
+        print "============"
     line = fatura_txt.readline()
 fatura_txt.close()
