43906a2f1375e560afa86618363cb473035e1d51
Diego Nogueira <diego.nogueira@loggi.com>
2018-12-28 20:23:24 -0200

    parsing txt works

--- parse_santander_fatura.py
+++ parse_santander_fatura.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 
 import csv
-import datetime
+from datetime import datetime
 import re
 
-transaction_pattern = re.compile("(\s*(\d{2}\/\d{2})\s+([\w\s\*-]+)\s+(PARC\s+(\d{2}\/\d{2}))?\s+([\d,\-\.]+))")
-installment_pattern = re.compile("(\d{2})\/\d{2}")
+transaction_pattern = re.compile("(\s*(\d{2}\/\d{2})\s+([\w\s\*\-]{25})\s+(PARC\s+(\d{2}\/\d{2}))?\s+([\d,\-\.]+))")
 
-fatura_txt = open("fatura.txt", "r")
-
-with open('ynab_input.csv', mode='w') as ynab_input:
+with open('ynab_input.csv', mode='w') as ynab_input, open("fatura.txt", "r") as fatura_txt:
     ynab_writer = csv.writer(ynab_input, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     ynab_writer.writerow(['Date', 'Payee', 'Memo','Outflow','Inflow'])
 
-    line = fatura_txt.readline()
-    while line:
+    for line in fatura_txt:
         for match in transaction_pattern.finditer(line):
-            transaction_date = match.group(2)
-            transaction_payee = match.group(3).strip()
+            date = datetime.strptime(match.group(2), '%d/%m').strftime('%m/%d/') + datetime.today().strftime('%Y')
+            payee = match.group(3).strip()
             transaction_value = match.group(6).replace(".","").replace(",",".")
-            installment = match.group(5)
-	    if (bool(installment)):
-                transaction_installment = "Installment " + installment_pattern.match(installment).group(1)
-            ynab_writer.writerow([transaction_date, transaction_payee, '',transaction_value,''])
-        line = fatura_txt.readline()
-    fatura_txt.close()
+            installment = match.group(4)
+            ynab_writer.writerow([date, payee, installment, transaction_value,''])
